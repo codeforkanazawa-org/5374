1193d4687dc5d5c8765a26832c174fd25d3348d1
yuki2006 <yagfair@gmail.com>
2013-08-11 03:28:16 +0900

    fix minor bug

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ $(function(){
 			  '<div class="accordion-group'+i+'">'+
     '<div class="accordion-heading">'+
       '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">'+
-        '<h2><p class="text-center">'+json[i].label+'</br></p></h2>'+
-        '<h4><p class="text-center">'+json[i].sublabel+'</br></p></h4>'+
+        '<h2><p class="text-center">'+data[i].label+'</br></p></h2>'+
+        '<h4><p class="text-center">'+data[i].sublabel+'</br></p></h4>'+
         '<h6><p class="text-left">2013.09.02 月曜日 AM 08:30</p></h6>'+
       '</a>'+
     '</div>'+
