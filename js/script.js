336b5a7429e93980636daece44768afc5738873a
yuki2006 <yagfair@gmail.com>
2013-08-17 23:00:23 +0900

    jsリファクタリング　と　表示の順番を直近の日時が近い方になるようにしました。

--- js/script.js
+++ js/script.js
@@ -1,9 +1,74 @@
-$(function() {
-  var list_data = new Array();
-  var center_data = new Array();
+var AreaModel = function() {
+  this.label;
+  this.centerName;
+  this.center;
+  this.trash = new Array();
+
+  this.calcMostRect = function() {
+    var now = new Date();
+    for (var i = 0; i < this.trash.length; i++) {
+      this.trash[i].calcMostRect(this);
+    }
+  }
+  this.isBlankDay = function(currentDate) {
+    var period = [this.center.startDate, this.center.endDate];
+
+    if (period[0].getTime() <= currentDate.getTime() &&
+      currentDate.getTime() <= period[1].getTime()) {
+      return true;
+    }
+    return false;
+  }
+  this.setCenter = function(center_data) {
+    for (var i in center_data) {
+      if (this.centerName == center_data[i].name) {
+        this.center = center_data[i];
+      }
+    };
+  }
+
+  this.sortTrash = function() {
+    this.trash.sort( function(a,b){
+      var at=a.mostRecent.getTime();
+      var bt=b.mostRecent.getTime();
+      if(  at< bt ) return -1;
+        if( at > bt) return 1;
+        return 0;
+    });
+  }
+
+}
+var Description = function() {
+  this.label;
+  this.notice;
+}
+var TrashModel = function(_lable, _cell) {
+  this.dayLabel;
+  this.mostRecent;
+  this.dayList;
+  this.dayCell = _cell.split(" ");
+  this.label = _lable;
+  this.description;
+
+  var result_text = "";
+
+  for (var j in this.dayCell) {
+    if (this.dayCell[j].length == 1) {
+      result_text += "毎週" + this.dayCell[j] + "曜日 ";
+    } else {
+      result_text += "第" + this.dayCell[j].charAt(1) + this.dayCell[j].charAt(0) + "曜日 ";
+    }
+  }
+  this.dayLabel = result_text;
+
+  this.getDateLabel = function() {
+
+    var result_text = this.mostRecent.getFullYear() + "/" + (1 + this.mostRecent.getMonth()) + "/" + this.mostRecent.getDate();
+
+    return this.dayLabel + " " + result_text;
+  }
+
   var day_enum = ["日", "月", "火", "水", "木", "金", "土"];
-  var descriptionClassification = new Array();
-  var area_days_label;
 
   function getDayIndex(str) {
     for (var i = 0; i < day_enum.length; i++) {
@@ -14,197 +79,213 @@ $(function() {
     return -1;
   }
 
-  function isBlankDay(currentDate, center_name) {
-    function getCenterData(center_name) {
-      for (var i = 0; i < center_data.length; i++) {
-        if (center_data[i][0] == center_name) {
-          return center_data[i];
+  this.calcMostRect = function(areaObj) {
+
+    var day_mix = this.dayCell;
+    var result_text = "";
+    var day_list = new Array();
+    var now = new Date();
+    // 12月 +3月　を表現
+    for (var month = 4; month <= 12 + 3; month++) {
+      for (var j in day_mix) {
+        //休止期間だったら、今後一週間ずらす。 
+        var isShift = false;
+
+        //week=0が第1週目です。
+        for (var week = 0; week < 5; week++) {
+
+          //4月1日を起点として第n曜日などを計算する。
+          var date = new Date(2013, month - 1, 1);
+          var d = new Date(date);
+          //毎週
+          if (day_mix[j].length == 1) {
+
+            //コンストラクタでやろうとするとうまく行かなかった。。
+            //
+            //4月1日を基準にして曜日の差分で時間を戻し、最大５週までの増加させて毎週を表現
+            d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
+              ((7 + getDayIndex(day_mix[j]) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
+            );
+            //同じ月の時
+            if ((d.getMonth() + 1) == month % 12) {
+              day_list.push(d) // += "<li>" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + "</li>";
+
+              if (this.mostRecent == null && d.getTime() > now.getTime()) {
+                this.mostRecent = d;
+              }
+
+            }
+
+          } else {
+
+            d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
+              ((7 + getDayIndex(day_mix[j].charAt(0)) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
+            );
+            //年末年始のずらしの対応
+            //休止期間なら、今後の日程を１週間ずらす
+            if (areaObj.isBlankDay(d, areaObj.centerName)) {
+              isShift = true;
+            }
+            if (isShift) {
+              d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
+            }
+            //特定の週のみ表示
+            if (week == day_mix[j].charAt(1) - 1) {
+              if (isShift) {
+                // console.log(d);
+              }
+              day_list.push(d) // += "<li>" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + "</li>";
+
+              if (this.mostRecent == null && d.getTime() > now.getTime()) {
+                this.mostRecent = d;
+              }
+            }
+          }
         }
-      };
-      return -1;
-    }
-
-    function getStartAndEndDay(center) {
-      function getDay(center, index) {
-        var tmp = center[index].split("/");
-        return new Date(tmp[0], tmp[1] - 1, tmp[2])
       }
-      return [getDay(center, 1), getDay(center, 2)];
     }
-
-    var center = getCenterData(center_name);
-    var period = getStartAndEndDay(center);
-
-    if (period[0].getTime() <= currentDate.getTime() &&
-      currentDate.getTime() <= period[1].getTime()) {
-      return true;
-    }
-    return false;
+    this.dayList = day_list;
   }
+  this.getDayList = function() {
+    var day_text = "<ul>";
+    for (var i in this.data_list) {
+      var d = data_list[i];
+      day_text += "<li>" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + "</li>"
+
+    };
+    day_text += "</ul>";
+    return day_text;
+  }
+
+}
+var CenterModel = function(row) {
+
+  function getDay(center, index) {
+    var tmp = center[index].split("/");
+    return new Date(tmp[0], tmp[1] - 1, tmp[2]);
+  }
+
+  this.name = row[0];
+  this.startDate = getDay(row, 1);
+  this.endDate = getDay(row, 2);
+
+
+}
+$(function() {
+  // var data_list = new Array();
+  var center_data = new Array();
+  var descriptions = new Array();
+  var area_days_label;
+  var areaModels = new Array();
 
   function update_area_list() {
     $.get("CSV/area_days.csv", function(csvdata) {
-        var csvdata = csvdata.replace("/¥r/gm", "");
-        var tmp = csvdata.split(String.fromCharCode(10));
-        area_days_label = tmp.shift().split(",");
+      var csvdata = csvdata.replace("/¥r/gm", "");
+      var tmp = csvdata.split(String.fromCharCode(10));
+      var area_days_label = tmp.shift().split(",");
+      for (var i in tmp) {
+        var row = tmp[i].split(",");
+        var area = new AreaModel();
+        area.label = row[0];
+        area.centerName = row[1];
+        areaModels.push(area);
+
+        for (var i = 2; i < 2 + 4; i++) {
+          var trash = new TrashModel(area_days_label[i], row[i]);
+          area.trash.push(trash);
+        }
+      }
+
+      $.get("CSV/center.csv", function(tmp_center_data) {
+        var tmp = tmp_center_data.split(String.fromCharCode(10));
         for (var i in tmp) {
           var row = tmp[i].split(",");
-          list_data.push(row);
+
+          var center = new CenterModel(row);
+          center_data.push(center);
         }
+        for (var i in areaModels) {
+          var area = areaModels[i];
+          area.setCenter(center_data);
+          // area.calcMostRect();
 
-        $.get("CSV/center.csv", function(tmp_center_data) {
-          var tmp = tmp_center_data.split(String.fromCharCode(10));
-          for (var i in tmp) {
-            var row = tmp[i].split(",");
-            center_data.push(row);
+        };
 
-            $("select.form-control").append('<option value="-1">未選択</option>');
-            for (var row_index in list_data) {
-              $("select.form-control").append("<option value=" + row_index + ">" + list_data[row_index][0] + "</option>");
-            }
-          }
-        });
+        $("select.form-control").append('<option value="-1">未選択</option>');
+        for (var row_index in areaModels) {
+          $("select.form-control").append("<option value=" + row_index + ">" + areaModels[row_index].label + "</option>");
+        }
       });
+    });
   }
 
   function create_menu_list(after_action) {
-    descriptionClassification = new Array();
-    $("#accordion").empty();
 
     $.getJSON("description.json", function(data) {
-        for (var i in data) {
-          descriptionClassification.push({
-            "label": data[i].label
-          });
-          var targets = data[i].target;
-          var target_tag = '<ul>';
-          for (var j in targets) {
-            target_tag += '<li>' + targets[j].name + '</li>';
-          }
-          target_tag += '</ul>';
+      for (var i in data) {
+        descriptions.push(data[i]);
+      }
 
-          $("#accordion").append(
-            '<div class="accordion-group' + i + '">' +
-            '<div class="accordion-heading">' +
-            '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse' + i + '">' +
-            '<h2><p class="text-center">' + '<center><img src="' + data[i].styles.svg + '" /></center>' + '</p></h2>' +
-            '<h4><p class="text-center">' + data[i].sublabel + '</p></h4>' +
-            '<h6><p class="text-left date"></p></h6>' +
-            '</a>' +
-            '</div>' +
-            '<div id="collapse' + i + '" class="accordion-body collapse">' +
-            '<div class="accordion-inner">' +
-            data[i].description + '<br />' + target_tag +
-            '<div class="targetDays"></div></div>' +
-            '</div>' +
-            '</div>');
-        }
-
-        after_action();
-      });
+      after_action();
+    });
   }
 
-  function update_date(row_index) {
-    var now = new Date();
+  function update_data(row_index) {
+    areaModels[row_index].calcMostRect();
+    //トラッシュの近い順にソートします。
+    areaModels[row_index].sortTrash();
+    //一旦削除
+    $("#accordion").empty();
 
-    for (var i = 2; i < list_data[row_index].length; i++) {
-      var day_list = "<ul>";
+    //アコーディオンの分類から対応の計算を行います。
+    for (var i in areaModels[row_index].trash) {
+      var description;
 
-      var day_mix = list_data[row_index][i].split(" ");
-      var result_text = "";
-      //直近の日時を更新
-      var mostRecent = null;
-      // 12月 +3月　を表現
-      for (var month = 4; month <= 12 + 3; month++) {
-        for (var j in day_mix) {
-          //休止期間だったら、今後一週間ずらす。 
-          var isShift = false;
-
-          //week=0が第1週目です。
-          for (var week = 0; week < 5; week++) {
-
-            //4月1日を起点として第n曜日などを計算する。
-            var date = new Date(2013, month - 1, 1);
-            var d = new Date(date);
-            //毎週
-            if (day_mix[j].length == 1) {
-
-              //コンストラクタでやろうとするとうまく行かなかった。。
-              //
-              //4月1日を基準にして曜日の差分で時間を戻し、最大５週までの増加させて毎週を表現
-              d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
-                ((7 + getDayIndex(day_mix[j]) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
-              );
-              //同じ月の時
-              if ((d.getMonth() + 1) == month % 12) {
-                day_list += "<li>" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + "</li>";
-
-                if (mostRecent==null && d.getTime() > now.getTime()) {
-                  mostRecent = d;
-                }
-
-              }
-
-            } else {
-
-              d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
-                ((7 + getDayIndex(day_mix[j].charAt(0)) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
-              );
-              //年末年始のずらしの対応
-              //休止期間なら、今後の日程を１週間ずらす
-              if (isBlankDay(d, list_data[row_index][1])) {
-                isShift = true;
-
-              }
-              if (isShift) {
-                d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
-              }
-              //特定の週のみ表示
-              if (week == day_mix[j].charAt(1) - 1) {
-                if (isShift) {
-                  // console.log(d);
-                }
-                day_list += "<li>" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + "</li>";
-
-                if (mostRecent==null && d.getTime() > now.getTime()) {
-                  mostRecent = d;
-                }
-
-              }
-            }
-          }
+      for (var j in descriptions) {
+        if (descriptions[j].label == areaModels[row_index].trash[i].label) {
+          description = descriptions[j];
         }
-      }
+      };
 
-      for (var j in day_mix) {
-        if (day_mix[j].length == 1) {
-          result_text += "毎週" + day_mix[j] + "曜日 ";
-        } else {
-          result_text += "第" + day_mix[j].charAt(1) + day_mix[j].charAt(0) + "曜日 ";
-        }
+      var target_tag = '<ul>';
+      var targets = description.target;
+      for (var j in targets) {
+        target_tag += '<li>' + targets[j].name + '</li>';
       }
-      result_text += mostRecent.getFullYear() + "/" + (1 + mostRecent.getMonth()) + "/" + mostRecent.getDate();
+      target_tag += '</ul>';
 
-      day_list += "</ul>"
+      $("#accordion").append(
+        '<div class="accordion-group' + i + '">' +
+        '<div class="accordion-heading">' +
+        '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse' + i + '">' +
+        '<h2><p class="text-center">' + '<center><img src="' + description.styles.svg + '" /></center>' + '</p></h2>' +
+        '<h4><p class="text-center">' + description.sublabel + '</p></h4>' +
+        '<h6><p class="text-left date"></p></h6>' +
+        '</a>' +
+        '</div>' +
+        '<div id="collapse' + i + '" class="accordion-body collapse">' +
+        '<div class="accordion-inner">' +
+        description.description + '<br />' + target_tag +
+        '<div class="targetDays"></div></div>' +
+        '</div>' +
+        '</div>');
 
-      //アコーディオンの分類から対応の計算を行います。
-      for (var label1 in descriptionClassification) {
-        if (descriptionClassification[label1].label == area_days_label[i]) {
-          $(".accordion-group" + (label1) + " .date").text(result_text);
-          $(".accordion-group" + (label1) + " .targetDays").html(day_list);
-          break;
-        }
-      }
+
+      var result_text = areaModels[row_index].trash[i].getDateLabel();
+      var day_list = areaModels[row_index].trash[i].getDayList();
+      $(".accordion-group" + (i) + " .date").text(result_text);
+      $(".accordion-group" + (i) + " .targetDays").html(day_list);
     }
-  } 
+  }
+
 
   function onChangeSelect(row_index) {
     if (row_index != -1 && $("#accordion").children().length == 0) {
       create_menu_list(function() {
-        update_date(row_index);
+        update_data(row_index);
       });
     } else {
-      update_date(row_index);
+      update_data(row_index);
     }
   }
 
