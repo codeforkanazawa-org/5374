d409d356059ee3559d27e25b9a468994ffd2da7f
yuki2006 <yagfair@gmail.com>
2013-09-03 11:35:50 +0900

    　見出しを大きくして、間隔を開けるなどの文字の調整を行いました。

--- js/script.js
+++ js/script.js
@@ -247,7 +247,7 @@ $(function() {
 
         var area_select_form = $("#select_area");
         var select_html = "";
-        select_html += '<option value="-1">未選択</option>';
+        select_html += '<option value="-1">地域を選択してください</option>';
         for (var row_index in areaModels) {
           var area_name = areaModels[row_index].label;
           var selected = (selected_name == area_name) ? 'selected="selected"' : '';
