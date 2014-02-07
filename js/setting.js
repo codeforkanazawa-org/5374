be51c6fc8966d877cc8b43a38dd356686e15b1a0
50river <mr.50river@gmail.com>
2014-02-07 17:12:18 +0900

    Revert "茨城町版からの機能追加によって付加された機能をmdに記述しました。"
    This reverts commit 223cc3a9bf5d893e918dcb9c2324ee0495a88b23.

--- js/setting.js
+++ js/setting.js
@@ -3,10 +3,10 @@
  *
 */
 
-const SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+const SVGLabel = true; // SVGイメージを使用するときは、true
 
-const MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+const MaxDescription = 9; // ごみの最大種類
 
 const MaxMonth = 3;
 
-const WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+const WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true
