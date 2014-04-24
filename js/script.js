71efb7c4bb4321257faf80155d3b19c243e01867
eoaoe <nulleins0802jp-2525@yahoo.co.jp>
2014-04-25 00:55:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -449,10 +449,10 @@ $(function() {
     areaModel.calcMostRect();
     //トラッシュの近い順にソートします。
     areaModel.sortTrash();
-    var accordion_height = $(".area").height() / descriptions.length;
+    var accordion_height = (window.innerHeight - 150) / descriptions.length;
     if(descriptions.length>4){
-      accordion_height = $(".area").height() / 4.1;
-      if (accordion_height>140) {accordion_height = $(".area").height() / descriptions.length;};
+      accordion_height = (window.innerHeight - 150) / 4.1;
+      if (accordion_height>140) {accordion_height = (window.innerHeight - 150) / descriptions.length;};
       if (accordion_height<130) {accordion_height=130;};
     }
     var styleHTML = "";
