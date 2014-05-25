1f34b55debc5ca671f3d259f9ebe8742fcf05a72
50jp <kksnc100@yahoo.co.jp>
2014-05-25 13:42:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ var TrashModel = function(_lable, _cell, remarks) {
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) == "*") {
     } else {
       // 不定期回収の場合（YYYYMMDD指定）
-      result_text = "毎日";
+      result_text = "不定期";
       this.regularFlg = 1;  // 定期回収フラグオフ：オンは　1　:オフは  0
     }
   }
