44def5df7749de7921e3ed8bf0a330971c451605
Okochama <okosukechan@gmail.com>
2014-12-13 12:48:08 +0900

    初期設定

--- js/setting.js
+++ js/setting.js
@@ -3,10 +3,10 @@
  *
 */
 
-var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
 var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 3;
 
-var WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
