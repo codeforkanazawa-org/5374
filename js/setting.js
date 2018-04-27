2bf4bd2858723c20df9ad17ac3386a1d15472ec9
kyounow <32561769+kyounow@users.noreply.github.com>
2018-04-27 13:53:49 +0900

    Add files via upload
    localize

--- js/setting.js
+++ js/setting.js
@@ -3,7 +3,7 @@
  *
 */
 
-var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
 var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
@@ -11,4 +11,4 @@ var MaxMonth = 3;
 
 var WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
 
-var SkipSuspend = true; // 休止期間を除去するときは、true。奈良の仕様は、true。
+var SkipSuspend = false; // 休止期間を除去するときは、true。奈良の仕様は、true。
